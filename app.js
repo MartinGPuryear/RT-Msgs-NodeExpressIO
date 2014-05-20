664b11086b7f20db3b10da180fc2f26618ddb9f3
Martin Puryear <martinpuryear@hotmail.com>
2014-05-20 01:59:06 -0700

    pushing to github

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
+
+//    Main page for RT-Messages application, but really 
+  //  just loads other modules.  Most of the server-
+  //  side code for this application is in route.js.
+
 var express = require('express.io');
 var path = require('path');
 var app = express().http().io();
 var port = 6789;
 
-        //    configure our environment
+    //    configure our environment
 app.configure(function()
   {
-    app.use(express.cookieParser());  
+  app.use(express.cookieParser());  
 
-    // app.use(express.bodyParser());                  
-        //    handle POST data
-        //    above is deprecated -- below will be in Express 3.0
-    app.use(express.urlencoded());
-    app.use(express.json());
+    //    handle POST data
+  // app.use(express.bodyParser());                  
+    //    above is deprecated -- below will be in Express 3.0
+  app.use(express.urlencoded());
+  app.use(express.json());
 
-        //    handle static contents
-    app.use(express.static(path.join(__dirname, 'public')));
+    //    handle static contents
+  app.use(express.static(path.join(__dirname, 'public')));
 
-        //    sessions
-    app.use(express.session({secret: 'peach'}));
+    //    sessions support enabled.
+  app.use(express.session({secret: 'peach'}));
 
-    app.set('view engine', 'ejs');
+  app.set('view engine', 'ejs');
   });
 
-        //    /routes/index.js handles all routing
+    //    /routes/route.js handles all routing
 var route = require('./routes/route.js')(app);
 app.listen(port);
 
--- routes/route.js
+++ routes/route.js
@@ -1,14 +1,19 @@
 module.exports = function Route(app)
 {
+
+  //  For this app, we store users in memory, not a DB
   var num_users = 0;
   var users = [];  
   
+  //  For this app, we store posts in memory, not a DB
   var num_posts = 0;
   var posts = [];  
 
+  //  For this app, we store replies in memory, not a DB
   var num_replies = 0;
   var replies = [];  
 
+  //  Create/insert a new user with this name and ID.
   function new_user(name, id)
   {
     var postArr = [];
@@ -16,19 +21,24 @@ module.exports = function Route(app)
     users.push( { name: name, user_id: id, posts: postArr, replies: replyArr });
   }
 
+  //  Create/insert a new post with this text, from this user.
   function new_post(text, post_id, name, user_id)
   {
       posts.push( { text: text, post_id: post_id, poster: name } );
       users[user_id].posts.push(text);
   }
 
+  //  Create/insert a new reply with this text, on this post, from this user.
   function new_reply(text, reply_id, post_id, name, user_id)
   {
     replies.push( { text: text, reply_id: reply_id, post_id: post_id, replier: name } );
     users[user_id].replies.push(text);
   }
 
-  function update_leaders()
+  //    Broadcast the top three leaders, in numbers of posts/replies.
+    //  For each user, insert them into a sorted list (capped at 3).
+    //  Then send these leaders to app.io (all). 
+  function update_leaders(request = null)
   {
     var user_num = 0;
     high_val = -1;
@@ -63,17 +73,32 @@ module.exports = function Route(app)
         }
       }
     }
-    app.io.broadcast('leaderboard_updated', { leaders: leaders });
+    if (request)
+    {
+      request.io.emit('leaderboard_updated', { leaders: leaders });
+    }
+    else
+    { 
+      app.io.broadcast('leaderboard_updated', { leaders: leaders });
+    }
   }
 
+
+  //  Route message handling
+
+  //    Client has connected, main page ready. Send out the leaderboard.
+    //  We send this out to the entire world, because the leaders
+    //  may have changed as a result of this user joining.  
   app.io.route('ready',      function(request) 
     { 
       update_leaders();
     });
 
+  //    Client loaded a profile. Send that profile's posts/replies.
+  //    Also, provide the leaderboard to that client only.
   app.io.route('profile_ready',      function(request) 
     { 
-      update_leaders();
+      update_leaders(request);
       var profile_id = request.session.visiting_profile_id;
       request.io.emit('user_post_list_received', { posts: users[profile_id].posts } );
       request.io.emit('user_reply_list_received', { replies: users[profile_id].replies } );
@@ -83,6 +108,7 @@ module.exports = function Route(app)
     {
     });
 
+  //    Client created a post.  Broadcast it, and an updated leaderboard. 
   app.io.route('new_post', function(request) 
     {
       num_posts++;
@@ -92,6 +118,7 @@ module.exports = function Route(app)
       update_leaders();
     });
 
+  //    Client created a reply.  Broadcast it, and an updated leaderboard. 
   app.io.route('new_reply', function(request)
     {
       num_replies++;
@@ -101,21 +128,40 @@ module.exports = function Route(app)
       update_leaders();
     });
 
+  //    Client requested the list of all posts - emit it.
   app.io.route('request_all_post_list', function(request) 
     {
       request.io.emit('all_post_list_received', { posts: posts })
     });
 
+  //    Client requested the list of all replies - emit it.
   app.io.route('request_all_reply_list', function(request) 
     {
       request.io.emit('all_reply_list_received', { replies: replies })
     });
 
+  //    Client requested the list of leaders - emit it.
   app.io.route('request_leaderboard', function(request) 
     {
       update_leaderboard();
     });
 
+
+  //  URL handling 
+
+  //    Root URL is routed to the index view, a login page.
+  app.get('/', function(request, response)
+    {
+      response.render('index', { title:'Enter the Wall', error: request.session.error });
+    });
+
+  //    Index URL is redirected to the root URL.
+  app.get('/index', function(request, response)
+    {
+      response.redirect('/');
+    });
+
+  //    Posting to /login creates a user, logs them in, redirects to wall. 
   app.post('/login', function(request, response)
     {
       if (request.body.name)
@@ -144,17 +190,17 @@ module.exports = function Route(app)
       }
     });
 
-  app.get('/', function(request, response)
-    {
-      response.render('index', { title:'Enter the Wall', error: request.session.error });
-    });
-
-  app.get('/wall', function(request,response)
+  //    The wall view is our real-time message board. All we really need 
+    //  to be passed is our name - the view requests other info it needs.
+  app.get('/wall', function(request, response)
     {
       response.render('wall', { title:'Real-Time Message Board', name:request.session.name });
     });
 
-  app.get('/profile/:user_name', function(request,response)
+  //    Profile view displays this user's name, the list of their posts,
+    //  and the list if their comments.  Like the wall view, all we
+    //  really need to pass is the name - the view will ask for the rest.  
+  app.get('/profile/:user_name', function(request, response)
     {
       for (var i = 0; i < users.length; i++) 
       {
@@ -179,6 +225,7 @@ module.exports = function Route(app)
       }
     });
 
+  //    User logged out/disconnected.  Clear out session, redirect to /.
   app.get('/logout', function(request, response)
     {
       request.session.name = null;
@@ -188,5 +235,4 @@ module.exports = function Route(app)
           response.redirect('/');
         }); 
     });
-
 }
